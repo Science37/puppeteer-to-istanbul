8de4902d68fd38d42421b60f246d9691b837657b
david.villarama <david.villarama@science37.com>
2019-01-07 11:45:16 -0800

    changed filter to be a method

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const PuppeteerToIstanbul = require('./lib/puppeteer-to-istanbul')
 module.exports = {
   /*
    * puppeteerFormat = puppeteer coverage
-   * output = optional string filename
-   * filter = optional url string array filter
+   * output - optional string output filename
+   * filter - optional method: takes the url and returns false to include url
    *
    */
   write: (puppeteerFormat, output, filter) => {
